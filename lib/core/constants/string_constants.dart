740d1cdadfea07c1c634a77e82d3cda4d8dfeb69
Gautam <gautamm@meditab.com>
2025-07-02 15:56:00 +0530

    Changes: strings are now centralized in a string constant for consistency and maintainability.

--- lib/core/constants/string_constants.dart
+++ lib/core/constants/string_constants.dart
@@ -22,9 +22,57 @@
 class StringC {
   StringC._();
 
+  /// region Utility Strings
   static const String dateFormat = "MMM dd, yyyy";
 
-  static const String name = 'Gautam Manwani';
-  static const String letWorkTogether = "Let's work together.";
+  /// endregion
+
+  /// region Header
+  static const String me = 'Me';
+  static const String work = 'Work';
+  static const String profile = 'Profile';
+  static const String contact = 'Contact';
+
+  /// endregion
+
+  /// region Landing
+  static const String fullName = 'Gautam Manwani';
+  static const String gautam = 'Gautam';
+  static const String manwani = 'Manwani';
+  static const String landingSubtitle =
+      'Flutter & Mobile Developer\nbuilding high-performance apps.';
+
+  /// endregion
+
+  /// region Work
+  static const String masterpieceStudio = 'MasterPiece\nStudio';
+  static const String masterpieceStudioSubtitle =
+      "A visual journey through world-class art, right from your fingertips.\nIt’s making fine art feel personal, accessible, and maybe even addictive.";
+  static const String dosePackDelivery = 'DosePack\nDelivery';
+  static const String dosePackDeliverySubtitle =
+      "Built for real-time logistics — maps, navigation, and status updates that don’t miss a beat.\nIt’s changing how essential meds reach homes, one seamless delivery at a time.";
+  static const String myDoses = 'MyDoses';
+  static const String myDosesSubtitle =
+      "Smart medication tracking that just works — even when your phone doesn’t.\nIt’s helping people stay on schedule, stay safe, and stay independent.";
+
+  /// endregion
+
+  /// region Contact
+  static const String medium = 'Medium';
+  static const String mediumUrl = 'http://gautam007.medium.com';
+  static const String email = 'Email';
+  static const String emailUrl = 'gautammanwani04@gmail.com';
+  static const String linkedIn = 'LinkedIn';
+  static const String linkedInUrl =
+      'https://www.linkedin.com/in/gautam-manwani-462495230/';
+  static const String stackOverflow = 'Stack Overflow';
+  static const String stackOverflowUrl =
+      'https://stackoverflow.com/users/21977414/pixel';
+
+  /// endregion
+
+  /// region Footer
   static const String craftedWithPassion = "@ Crafted By Me with Passion.";
+
+  /// endregion
 }
