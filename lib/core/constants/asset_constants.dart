74fe93a8b944de2d5af757fefb9d5c8597b49342
Gautam <gautamm@meditab.com>
2025-12-02 14:40:55 +0530

    feat added: New assets.

--- lib/core/constants/asset_constants.dart
+++ lib/core/constants/asset_constants.dart
@@ -6,7 +6,22 @@ class AssetC {
   static const String dosePackDelivery = "$imagePath/doses_delivery.jpg";
   static const String myDoses = "$imagePath/my_doses.jpg";
   static const String masterpieceStudio = "$imagePath/masterpiece_studio.jpg";
+  static const String cataractAI = "$imagePath/cataract_ai.jpg";
+  static const String showItMax = "$imagePath/showitmax.jpg";
   static const String projectGroup = "$imagePath/project_group.png";
+  static const String apple = "$imagePath/apple.png";
+  static const String google = "$imagePath/google.png";
+
+  // endregion
+
+  // region Svgs
+  static const String svgPath = "assets/svg";
+  static const String linkedin = "$svgPath/linkedin.svg";
+  static const String stackOverflow = "$svgPath/stack-overflow.svg";
+  static const String medium = "$svgPath/medium.svg";
+  static const String resume = "$svgPath/file-user.svg";
+  static const String playStore = "$svgPath/play-store.svg";
+  static const String appStore = "$svgPath/app-store.svg";
 
   // endregion
 
